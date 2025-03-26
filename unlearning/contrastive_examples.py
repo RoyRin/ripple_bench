521039706353874d11b4955a7e0333a0deded4b5
Roy Rinberg <royrinberg@g.harvard.edu>
2025-03-26 12:13:44 -0400

    cleaning up LEACE_exploration notebook

--- unlearning/contrastive_examples.py
+++ unlearning/contrastive_examples.py
@@ -11,7 +11,7 @@ import os
 from unlearning import probes
 
 from collections import defaultdict
-
+from matplotlib import pyplot as plt
 
 class ImageSearch:
     def __init__(self, model, layer_ind, embedding_size=2048):
@@ -65,7 +65,7 @@ def build_positive_examples_db(probe_dataset, probe_labels, model, layer_ind):
 
     N_ = len(probe_dataset)
     for ii, (embedding, embedding_label) in enumerate(zip(probe_dataset, probe_labels)):
-        if embedding_label == 0:
+        if embedding_label == 0: # skip negative labels
             continue
         if ii % (N_//10) == 0:
             print(f"embedding {ii}/{N_}")
@@ -73,15 +73,55 @@ def build_positive_examples_db(probe_dataset, probe_labels, model, layer_ind):
     # add an embedding
     return search_engine
 
-def find_closest_examples(search_engine, probe_dataset, probe_labels, N = 1000):
+def build_positive_examples_db_from_model(dataset, model, layer_ind, positive_indices, device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    model_layer_count = probes.get_layer_count(model)
+    print(f"model_layer_count- {model_layer_count}")
+    print(f"init")
+
+    embedding_size = probes.get_embedding_size(model, layer_ind = layer_ind)
+
+    print(f"model embeddinging size {embedding_size}")
+    # compute the embedding size 
+    search_engine = ImageSearch(model = model, layer_ind = layer_ind, embedding_size=embedding_size)
+
+    N_ = len(positive_indices)
+
+    embedding_counts = 0    
+    for ii, (image_ind) in enumerate(positive_indices):
+        embedding_counts += 1
+
+        image, label = dataset[image_ind]
+
+        img_tensor = image.unsqueeze(0).to(device)  # Add batch dimension
+        #embedding = get_nth_to_last_embedding(model.to(device), img_tensor, layer_ind=layer_ind)
+        embedding = probes.get_flattened_embedding(model.to(device), img_tensor, layer_ind=layer_ind)
+        embedding = embedding.squeeze(0)
+        
+        if ii % (N_//10) == 0:
+            print(f"embedding {ii}/{N_}; embedding_counts {embedding_counts}")
+        search_engine.add_embedding(embedding = embedding, image_index = ii)
+    # add an embedding
+
+    return search_engine
+
+def find_closest_examples(search_engine, dataset, neg_indices, layer_ind, model, top_N = 500, N_to_search = 1_000, device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
     nearest_distances = defaultdict(lambda: 1000000)
 
-    for ii, (embedding, embedding_label) in enumerate(zip(probe_dataset, probe_labels)):
-        if embedding_label == 1: # only negative labels
-            continue
-        if ii % (N//10) == 0:
-            print(f"embedding {ii}/{N}")
-        
+    for ii, (index) in enumerate(neg_indices):
+        image, label = dataset[index]
+        if ii >= N_to_search:
+            break
+
+        if ii % (N_to_search//10) == 0:
+            print(f"embedding {ii}/{N_to_search}")
+
+        img_tensor = image.unsqueeze(0).to(device)  # Add batch dimension
+        #embedding = get_nth_to_last_embedding(model.to(device), img_tensor, layer_ind=layer_ind)
+        embedding = probes.get_flattened_embedding(model.to(device), img_tensor, layer_ind=layer_ind)
+        embedding = embedding.squeeze(0)
+
+        #embedding = probes.get_flattened_embedding(model = search_engine.model, image = image.unsqueeze(0), layer_ind = search_engine.layer_ind).squeeze(0)
+
         results = search_engine.search_embedding(query_embedding = embedding, top_k=5)
         for (ind, dist) in results:
             if ind == ii:
@@ -89,7 +129,8 @@ def find_closest_examples(search_engine, probe_dataset, probe_labels, N = 1000):
             nearest_distances[ind] = min(nearest_distances[ind], dist)
 
     ordered_nearest_distances = sorted(nearest_distances.items(), key=lambda x: x[1])
-    closest_neg_indices = [x[0] for x in ordered_nearest_distances[:N]]
+    print(f"ordered_nearest_distances- len{len(ordered_nearest_distances)}")
+    closest_neg_indices = [x[0] for x in ordered_nearest_distances[:top_N]]
     return closest_neg_indices
 
 
@@ -122,28 +163,32 @@ def create_probe_dataset_random_points(dataset, labels, model, layer_ind, attrib
 
 
 
-def create_probe_dataset_contrastive_examples(dataset, labels, model, layer_ind, attribute_index, N = 1000):
-    """
-    Construct a dataset for the probe by randomly selecting all the positive points, and the closest negative examples to the positive examples, in the dataset validation dataset.
-    """
 
-    positive_attributes = labels[:, attribute_index] == 1
-    negative_attributes = labels[:, attribute_index] == 0
-    #
-    pos_indices = torch.where(positive_attributes)[0]
-    neg_indices = torch.where(negative_attributes)[0]
-    #    
-    # shuffle pos_indices
-    np.random.shuffle(pos_indices)
-    np.random.shuffle(neg_indices)
 
-    # search the negative examples, for closest examples to the positive examples 
-    search_engine = build_positive_examples_db(dataset, labels, model, layer_ind)
-    closest_neg_indices = find_closest_examples(search_engine, dataset, labels, N = N)
 
-    
-    # create probe dataset of these closest_neg_attributes, and positive images
+def search_demo(search_engine, probe_dataset, probe_labels, N = 1000):
+        # test it out 
+    search_engine.search_embedding(query_embedding =  probe_dataset[-1], top_k=5)
+    from collections import defaultdict
+    seen = set()
+    nearest_distances = defaultdict(lambda: 1000000)
 
-    closest_probe_dataset, closest_probe_dataset_labels = probes.set_up_probe_dataset(model, layer_ind, pos_indices=pos_indices, neg_indices=closest_neg_indices, dataset = dataset, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), verbose = True)
-    return closest_probe_dataset, closest_probe_dataset_labels
+    for ii, (embedding, embedding_label) in enumerate(zip(probe_dataset, probe_labels)):
+        if embedding_label == 1:
+            continue
+        if ii % (N//10) == 0:
+            print(f"embedding {ii}/{N}")
+        
+        results = search_engine.search_embedding(query_embedding = embedding, top_k=5)
+        for (ind, dist) in results:
+            if ind == ii:
+                continue
+            nearest_distances[ind] = min(nearest_distances[ind], dist)
 
+    # plot hist of nearest_distances
+
+    plt.hist(list(nearest_distances.values()), bins=100)
+    plt.xlabel("Distance")
+    plt.ylabel("Count")
+    plt.title("Histogram of Nearest Distances")
+    plt.show()
