5d72c0958b81b3363ee3a211f770df7df0ea4ebc
Roy Rinberg <royrinberg@gmail.com>
2025-04-22 20:15:50 -0400

    make openai wrapper using HUIT key

---
+++ unlearning/openai_utils.py
@@ -0,0 +1,35 @@
+
+import requests
+import json
+
+def get_open_ai_huit_secret(SECRET_DIR):
+    SECRETS = SECRET_DIR / "openai_huit.secret"
+    # open SECRETS
+    with open(SECRETS, "r") as f:
+        OPEN_AI_key = f.read().strip()
+    return OPEN_AI_key
+
+
+
+def make_openai_request(prompt, OPEN_AI_key, model = "gpt-4o-mini", temperature = 0.75):
+    
+    url = "https://go.apis.huit.harvard.edu/ais-openai-direct/v1/chat/completions"
+    payload = json.dumps({
+    "model": model,
+    "messages": [
+        {
+        "role": "user",
+        "content": prompt
+        }
+    ],
+    "temperature": temperature
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'api-key': OPEN_AI_key
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    content = json.loads(response.text)["choices"][0]["message"]["content"] 
+
+    return content
